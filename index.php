2f5845c445fe1b9a2930c28554b0341423d7c456
SimoneLollini <lollinisimone@gmail.com>
2022-12-07 17:15:29 +0100

    wrapped in .card

--- index.php
+++ index.php
@@ -55,38 +55,38 @@ $MovieList = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
+    <link rel="stylesheet" href=" 	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <?php foreach ($MovieList as $movie) : ?>
-        <div class="card">
-
-            <div class="title">
-                <?php echo $movie->title ?>
-            </div>
-            <div class="description">
-                <?php echo $movie->description ?>
-
-            </div>
-            <div class="lang">
-                <?php echo $movie->language ?>
-
-            </div>
-            <div class="genre">
-                <?php echo $movie->genre ?>
-
-            </div>
-            <div class="length">
-                <?php echo $movie->length ?>
-
-            </div>
-            <div class="rating">
-                <?php echo $movie->vote ?>
-
-            </div>
-
+    <div class="container">
+        <div class="row pt-5">
+            <?php foreach ($MovieList as $movie) : ?>
+                <div class="col">
+                    <div class="card text-center">
+                        <div class="title">
+                            Titolo: <?php echo $movie->title ?>
+                        </div>
+                        <div class="description">
+                            Descrizione: <?php echo $movie->description ?>
+                        </div>
+                        <div class="lang">
+                            Lingua originale: <?php echo $movie->language ?>
+                        </div>
+                        <div class="genre">
+                            Genere <?php echo $movie->genre ?>
+                        </div>
+                        <div class="length">
+                            Durata: <?php echo $movie->length ?>
+                        </div>
+                        <div class="rating">
+                            Voto: <?php echo $movie->vote ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
         </div>
-    <?php endforeach; ?>
+    </div>
 </body>
 
 </html>
