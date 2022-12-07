24ce3c2a403b88fb21dbcd59dbe99fd88e189b54
SimoneLollini <lollinisimone@gmail.com>
2022-12-07 15:30:31 +0100

    Add contructor to class Movie

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
-<!-- è definita una classe ‘Movie’
-all'interno della classe sono dichiarate delle variabili d'istanza
-all'interno della classe è definito un costruttore
-all'interno della classe è definito almeno un metodo
-vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-Bonus 1:
-Modificare la classe Movie in modo che accetti piú di un genere.
-Bonus 2:
-Creare un layout completo per stampare a schermo una lista di movies. -->
+<!-- all'interno della classe è definito almeno un metodo -->
+<!-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+<!-- Bonus 1: -->
+<!-- Modificare la classe Movie in modo che accetti piú di un genere. -->
+<!-- Bonus 2: -->
+<!-- Creare un layout completo per stampare a schermo una lista di movies. -->
 
 <?php
+class Movie
+{
+    public $title;
+    public $description;
+    public $language;
+    public $length;
+    public $vote;
+    function __construct($title, $description, $language, $length, $vote)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->language = $language;
+        $this->length = $length;
+        $this->vote = $vote;
+    }
+}
+var_dump(new Movie('matrix', 'desc', 'lang', 'length', 'vote'));
